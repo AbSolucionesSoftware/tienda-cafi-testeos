26a5807948098b4acf8675a07d7d3263b178b989
AldoChagollan <aldochagollan@outlook.com>
2020-11-24 09:41:42 -0600

    cambios filtros, registro categorias

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8daaf629bea7948322402c7c10362a0e.js"
+  "/precache-manifest.90b2e8ce81cd912a6fa0c8a74cb8af1d.js"
 );
 
 self.addEventListener('message', (event) => {
